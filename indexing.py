1588c35cc4237100a1218aae9feaf7d31ef3507f
Esther <160644164+esthergreene@users.noreply.github.com>
2025-09-23 23:28:50 -0400

    VariableLengthRecord - my head hurts

--- indexing.py
+++ indexing.py
@@ -20,3 +20,43 @@ https://www.geeksforgeeks.org/python/struct-module-python/
 import struct, os
 
 PAGE_SIZE, HEADER_SIZE, SLOT_SIZE = 4096, 4, 8
+
+"""
+References: 
+https://www.geeksforgeeks.org/python/__init__-in-python/
+https://www.tutorialspoint.com/python/python_serialization.htm
+https://www.geeksforgeeks.org/python/serialize-and-deserialize-complex-json-in-python/
+https://stackoverflow.com/questions/63228415/def-serializeself-what-does-it-do-in-django-models
+https://stackoverflow.com/questions/45232298/null-bitmap-is-set-when-there-is-no-null-columns
+https://www.geeksforgeeks.org/python/enumerate-in-python/
+https://www.geeksforgeeks.org/python/python-check-if-nonetype-or-empty/
+https://www.w3schools.com/python/ref_list_append.asp
+https://www.w3schools.com/python/python_conditions.asp
+https://www.w3schools.com/python/ref_string_encode.asp
+https://www.geeksforgeeks.org/python/python-pack-method-in-tkinter/
+https://www.geeksforgeeks.org/python/classmethod-in-python/
+https://docs.python.org/3/library/struct.html
+https://www.tutorialspoint.com/python/string_decode.htm
+"""
+class VariableLengthRecord:
+    def __init__(self, id, name, dept, salary): self.id, self.name, self.dept, self.salary = id, name, dept, salary
+    def serialize(self):
+        nullBitmap=0; emptyList=[]
+        for i, v in enumerate([self.id, self.name, self.dept]):
+            if v is None: nullBitmap|=1<<i; emptyList.append(b'')
+            else: emptyList.append(str(v).encode())
+        if self.salary is None: nullBitmap|=1<<3; sal=0.0
+        else: sal=float(self.salary)
+        offsets,lengths,offsetCounter=[],[],0
+        for f in emptyList:
+            offsets.append(offsetCounter if f else 0); lengths.append(lengths(f)); offsetCounter+=lengths(f)
+        out=[struct.pack("<B",nullBitmap)]+[struct.pack("<I",x) for x in sum(zip(offsets,lengths),())]+[struct.pack("<f",sal),b''.join(emptyList)]
+        return b''.join(out)
+    @classmethod
+    def deserialize(classMethod, data):
+        nullBitmap=data[0]; offsets=[struct.unpack("<I",data[1+8*i:5+8*i])[0] for i in range(3)]
+        lengths=[struct.unpack("<I",data[5+8*i:9+8*i])[0] for i in range(3)]
+        sal=struct.unpack("<f",data[25:29])[0]; vd=data[29:]
+        get=lambda i: None if nullBitmap>>i&1 else vd[offsets[i]:offsets[i]+lengths[i]].decode()
+        return classMethod(get(0),get(1),get(2),None if nullBitmap>>3&1 else sal)
+    def __repr__(self): return f"({self.id},{self.name},{self.dept},{self.salary})"
