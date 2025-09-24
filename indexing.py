a5644aa73b94f4c53210154e9dceb4fbc1ac9771
Esther <160644164+esthergreene@users.noreply.github.com>
2025-09-23 22:44:40 -0400

    admin work

--- indexing.py
+++ indexing.py
@@ -1,1 +1,10 @@
-print("This is a test to make sure GitHub is synced correctly.")
+"""
+ * Name: indexing.py
+ * Author: esthergreene
+ * Project: Homework 1
+ * Version: 09/23/2025
+ * Purpose: Implement a Python-based storage system for variable-length records using the
+ *    slotted-page structure as described in database systems theory. You will create a system
+ *    that can store, retrieve, and manage records with variable-length fields within fixed-size pages.
+ * Note: Sources have been cited and appear above respective code (e.g., "References:")
+"""
